7545871853540c4c72bfd6d8cb5d77b5f8b362dc
WallochMatt <matthewrwalloch@gmail.com>
2023-04-19 14:55:32 -0500

    feat: Completed problem 16 + Updated Product 10's price

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -28,9 +28,9 @@ namespace DatabaseFirstLINQ
             //ProblemEleven();
             //ProblemTwelve();
             //ProblemThirteen();
-            ProblemFourteen();
+            //ProblemFourteen();
             //ProblemFifteen();
-            //ProblemSixteen();
+            ProblemSixteen();
             //ProblemSeventeen();
             //ProblemEighteen();
             //ProblemNineteen();
@@ -239,7 +239,10 @@ namespace DatabaseFirstLINQ
         private void ProblemSixteen()
         {
             // Update the price of the product you created to something different using LINQ.
-
+            var product = _context.Products.Where(prod => prod.Name == "Google Pixel 7").SingleOrDefault();
+            product.Price = 500;
+            _context.Products.Update(product);
+            _context.SaveChanges();
         }
 
         private void ProblemSeventeen()
